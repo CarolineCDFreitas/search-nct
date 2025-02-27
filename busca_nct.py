3aadda282ed3bbbc751e39bb4f6109ddf691ae70
CarolineCaetano <caroline_bdl@hotmail.com>
2025-02-27 15:06:38 -0300

    add id| feature: filtro para evitar duplicação

--- busca_nct.py
+++ busca_nct.py
@@ -11,31 +11,31 @@ class OPCOES(Enum):
 
 
 # membros
-johnny = ["Johnny", "1995", "2017"]
-taeyong = ["Taeyong", "1995", "2016"]
-yuta = ["Yuta", "1995", "2016"]
-doyoung = ["Doyoung", "1996", "2016"]
-kun = ["Kun", "1996", "2018"]
-ten = ["Ten", "1996", "2016"]
-jaehyun = ["Jaehyun", "1997", "2016"]
-winwin = ["Winwin", "1997", "2016"]
-jungwoo = ["Jungwoo", "1998", "2018"]
-mark = ["Mark", "1999", "2016"]
-hendery = ["Hendery", "1999", "2019"]
-xiaojun = ["Xiaojun", "1999", "2019"]
-haechan = ["Haechan", "2000", "2016"]
-renjun = ["Renjun", "2000", "2016"]
-jeno = ["Jeno", "2000", "2016"]
-jaemin = ["Jaemin", "2000", "2016"]
-yangyang = ["YangYang", "2000", "2019"]
-chenle = ["Chenle", "2001", "2016"]
-jisung = ["Jisung", "2002", "2016"]
-sion = ["Sion", "2002", "2024"]
-riku = ["Riku", "2003", "2024"]
-yushi = ["Yushi", "2005", "2024"]
-jaehee = ["Jaehee", "2005", "2024"]
-ryo = ["Ryo", "2007", "2024"]
-sakuya = ["Sakuya", "2007", "2024"]
+johnny = [1, "Johnny", "1995", "2017"]
+taeyong = [2, "Taeyong", "1995", "2016"]
+yuta = [3, "Yuta", "1995", "2016"]
+doyoung = [4, "Doyoung", "1996", "2016"]
+kun = [5, "Kun", "1996", "2018"]
+ten = [6, "Ten", "1996", "2016"]
+jaehyun = [7, "Jaehyun", "1997", "2016"]
+winwin = [8, "Winwin", "1997", "2016"]
+jungwoo = [9, "Jungwoo", "1998", "2018"]
+mark = [10, "Mark", "1999", "2016"]
+hendery = [11, "Hendery", "1999", "2019"]
+xiaojun = [12, "Xiaojun", "1999", "2019"]
+haechan = [13, "Haechan", "2000", "2016"]
+renjun = [14, "Renjun", "2000", "2016"]
+jeno = [15, "Jeno", "2000", "2016"]
+jaemin = [16, "Jaemin", "2000", "2016"]
+yangyang = [17, "YangYang", "2000", "2019"]
+chenle = [18, "Chenle", "2001", "2016"]
+jisung = [19, "Jisung", "2002", "2016"]
+sion = [20, "Sion", "2002", "2024"]
+riku = [21, "Riku", "2003", "2024"]
+yushi = [22, "Yushi", "2005", "2024"]
+jaehee = [23, "Jaehee", "2005", "2024"]
+ryo = [24, "Ryo", "2007", "2024"]
+sakuya = [25, "Sakuya", "2007", "2024"]
 
 
 # nct
@@ -64,25 +64,28 @@ while True:
     print("Opções \n1 - Ano de debut \n2 - Unit \n3 - Nome \n4 - Idade \n")
     opcao = int(input("Insira a opção que deseje pesquisar: "))
 
-    members = []
+    id_membro = []
+    membros_encontrados = []
     if opcao == OPCOES.ANO_DE_DEBUT.value:
         resposta = int(input("Digite o ano: "))
         if resposta:
             for unit in nct:
-                for member in unit:
-                    if isinstance(member, list):
-                        if resposta == int(member[2]):
-                            members.append(member[0])
+                for membro in unit:
+                    if isinstance(membro, list):
+                        if resposta == int(membro[3]):
+                            id_membro.append(membro[0])
+                            membros_encontrados.append(membro[1])
 
-    if len(members) >= 2:
-        members.insert(-1, "e")
-        list = ", ".join(members).replace(", e,", " e")
-        
-    
+    membros_id = list(set(id_membro))
+    membros = list(set(membros_encontrados))
 
-    if (len(members)) == 1:
-        print(f"O membro do NCT que debutou no ano {str(resposta)} foi {members[0]}.")
-    elif (len(members)) > 1:
+    if len(membros) >= 2:
+        membros.insert(-1, "e")
+        list = ", ".join(membros).replace(", e,", " e")
+
+    if (len(membros)) == 1:
+        print(f"O membro do NCT que debutou no ano {str(resposta)} foi {membros[0]}.")
+    elif (len(membros)) > 1:
         print(f"Os membros do NCT que debutaram no ano {str(resposta)} foram {list}.")
     elif resposta < 2016:
         print(
@@ -91,10 +94,3 @@ while True:
     else:
         print("Nenhum membro debutou nesse ano.")
     break
-
-
-# for unit in nct:
-#     for member in unit:
-#         if isinstance(member, list):
-#             if int(member[1]) > 2000:
-#                 print(f"{member[0]}, nascido em {member[1]} do {unit[0]}")
