817c942fcea7cfc09f40cd887f6c1677204bfbde
CarolineCaetano <caroline_bdl@hotmail.com>
2025-03-08 12:22:06 -0300

    feat: atualização estrutural e funcional de listas para dicionários

--- busca_nct.py
+++ busca_nct.py
@@ -1,4 +1,4 @@
-# exercício de busca com lista, loops e condicionais
+# exercício de busca com lista, loops e condicionais. Agora dicionários
 
 from enum import Enum
 
@@ -7,58 +7,334 @@ class OPCOES(Enum):
     ANO_DE_DEBUT = 1
     UNIT = 2
     NOME = 3
-    AGE = 4
+    IDADE = 4
 
+#membros
+johnny = {
+    "id": 1,
+    "nome_artistico": "Johnny",
+    "nome": "John Jun Suh",
+    "ano_de_nascimento": "1995",
+    "nacionalidade": "estadunidense",
+    "debut_info": [
+        {"ano": 2017, "unit": "127", "debut_song": "Limitless"},
+    ],
+}
 
-# membros
-johnny = [1, "Johnny", "1995", "2017"]
-taeyong = [2, "Taeyong", "1995", "2016"]
-yuta = [3, "Yuta", "1995", "2016"]
-doyoung = [4, "Doyoung", "1996", "2016"]
-kun = [5, "Kun", "1996", "2018"]
-ten = [6, "Ten", "1996", "2016"]
-jaehyun = [7, "Jaehyun", "1997", "2016"]
-winwin = [8, "Winwin", "1997", "2016"]
-jungwoo = [9, "Jungwoo", "1998", "2018"]
-mark = [10, "Mark", "1999", "2016"]
-hendery = [11, "Hendery", "1999", "2019"]
-xiaojun = [12, "Xiaojun", "1999", "2019"]
-haechan = [13, "Haechan", "2000", "2016"]
-renjun = [14, "Renjun", "2000", "2016"]
-jeno = [15, "Jeno", "2000", "2016"]
-jaemin = [16, "Jaemin", "2000", "2016"]
-yangyang = [17, "YangYang", "2000", "2019"]
-chenle = [18, "Chenle", "2001", "2016"]
-jisung = [19, "Jisung", "2002", "2016"]
-sion = [20, "Sion", "2002", "2024"]
-riku = [21, "Riku", "2003", "2024"]
-yushi = [22, "Yushi", "2005", "2024"]
-jaehee = [23, "Jaehee", "2005", "2024"]
-ryo = [24, "Ryo", "2007", "2024"]
-sakuya = [25, "Sakuya", "2007", "2024"]
+taeyong = {
+    "id": 2,
+    "nome_artistico": "Taeyong",
+    "nome": "Lee Taeyong",
+    "ano_de_nascimento": "1995",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [
+        {"ano": 2016, "unit": "u", "debut_song": "The 7th Sense"},
+        {"ano": 2016, "unit": "127", "debut_song": "Fire Truck"},
+        {"ano": 2023, "unit": "solo", "debut_song": "Shalala"},
+    ],
+}
 
-debut_years = ["2016", "2017", "2018", "2019", "2024"]
+yuta = {
+    "id": 3,
+    "nome_artistico": "Yuta",
+    "nome": "Nakamoto Yuta",
+    "ano_de_nascimento": "1995",
+    "nacionalidade": "japonês",
+    "debut_info": [
+        {"ano": 2016, "unit": "127", "debut_song": "Fire Truck"},
+        {"ano": 2024, "unit": "solo", "debut_song": "Off the Mask"},
+    ],
+}
 
-# nct
-dojaejung = ["DOJAEJUNG", doyoung, jaehyun, jungwoo, "nct", "2023"]
-ilichil = [
-    "127",
-    johnny,
-    taeyong,
-    yuta,
-    doyoung,
-    jaehyun,
-    jungwoo,
-    mark,
-    haechan,
-    "nct",
-    "2016",
-]
-dream = ["dream", mark, haechan, renjun, jeno, jaemin, chenle, jisung, "nct", "2016"]
-wayv = ["wayv", kun, ten, winwin, hendery, xiaojun, yangyang, "nct", "2019"]
-wish = ["wish", sion, riku, yushi, jaehee, ryo, sakuya, "nct", "2024"]
-nct = [dojaejung, ilichil, dream, wayv, wish]
+doyoung = {
+    "id": 4,
+    "nome_artistico": "Doyoung",
+    "nome": "Kim Dongyoung",
+    "ano_de_nascimento": "1996",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [
+        {"ano": 2016, "unit": "u", "debut_song": "The 7th Sense"},
+        {"ano": 2017, "unit": "127", "debut_song": "Limitless"},
+        {"ano": 2023, "unit": "dojaejung", "debut_song": "Perfume"},
+        {"ano": 2024, "unit": "solo", "debut_song": "Little Light"},
+    ],
+}
 
+kun = {
+    "id": 5,
+    "nome_artistico": "Kun",
+    "nome": "Qian Kun",
+    "ano_de_nascimento": "1996",
+    "nacionalidade": "chinês",
+    "debut_info": [
+        {"ano": 2018, "unit": "u", "debut_song": None},
+        {"ano": 2019, "unit": "wayv", "debut_song": "Regular"},
+    ],
+}
+
+ten = {
+    "id": 6,
+    "nome_artistico": "Ten",
+    "nome": "Chittaphon Leechaiyapornkul",
+    "ano_de_nascimento": "1996",
+    "nacionalidade": "tailândes",
+    "debut_info": [
+        {"ano": 2016, "unit": "u", "debut_song": "The 7th Sense"},
+        {"ano": 2019, "unit": "wayv", "debut_song": "Regular"},
+        {"ano": 2024, "unit": "solo", "debut_song": "Nightwalker"},
+    ],
+}
+
+jaehyun = {
+    "id": 7,
+    "nome_artistico": "Jaehyun",
+    "nome": "Jeong Yuno",
+    "ano_de_nascimento": "1997",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [
+        {"ano": 2016, "unit": "u", "debut_song": "The 7th Sense"},
+        {"ano": 2016, "unit": "127", "debut_song": "Fire Truck"},
+        {"ano": 2023, "unit": "dojaejung", "debut_song": "Perfume"},
+        {"ano": 2024, "unit": "solo", "debut_song": "Smoke"},
+    ],
+}
+
+winwin = {
+    "id": 8,
+    "nome_artistico": "Winwin",
+    "nome": "Dong Sicheng",
+    "ano_de_nascimento": "1997",
+    "nacionalidade": "chinês",
+    "debut_info": [
+        {"ano": 2016, "unit": "127", "debut_song": "Fire Truck"},
+        {"ano": 2019, "unit": "wayv", "debut_song": "Regular"},
+    ],
+}
+
+jungwoo = {
+    "id": 9,
+    "nome_artistico": "Jungwoo",
+    "nome": "Kim Jungwoo",
+    "ano_de_nascimento": "1998",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [
+        {"ano": 2018, "unit": "u", "debut_song": "Boss"},
+        {"ano": 2018, "unit": "127", "debut_song": "Regular"},
+        {"ano": 2023, "unit": "dojaejung", "debut_song": "Perfume"},
+        {"ano": None, "unit": None, "debut_song": None},
+    ],
+}
+
+mark = {
+    "id": 10,
+    "nome_aristitico": "Mark",
+    "nome": "Mark Lee",
+    "ano_de_nascimento": "1999",
+    "nacionalidade": "canadense",
+    "debut_info": [
+        {"ano": 2016, "unit": "u", "debut_song": "The 7th Sense"},
+        {"ano": 2016, "unit": "127", "debut_song": "Fire Truck"},
+        {"ano": 2016, "unit": "dream", "debut_song": "Chewing Gum"},
+        {"ano": 2025, "unit": "solo", "debut_song": None},
+    ],
+}
+
+hendery = {
+    "id": 11,
+    "nome_artistico": "Hendery",
+    "nome": "Wong Kung Hang",
+    "ano_de_nascimento": "1999",
+    "nacionalidade": "macaense",
+    "debut_info": [
+        {"ano": 2019, "unit": "wayv", "debut_song": "Regular"},
+    ],
+}
+
+xiaojun = {
+    "id": 12,
+    "nome_artistico": "Xioajun",
+    "nome": "Xiao Dejun",
+    "ano_de_nascimento": "1999",
+    "nacionalidade": "chinês",
+    "debut_info": [
+        {"ano": 2019, "unit": "wayv", "debut_song": "Regular"},
+        {"ano": 2020, "unit": "u", "debut_song": "Make a Wish"},
+    ],
+}
+
+haechan = {
+    "id": 13,
+    "nome_artistico": "Haechan",
+    "nome": "Lee Donghyuck",
+    "ano_de_nascimento": "2000",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [
+        {"ano": 2016, "unit": "127", "debut_song": "Fire Truck"},
+        {"ano": 2016, "unit": "dream", "debut_song": "Chewing Gum"},
+        {"ano": None, "unit": "solo", "debut_song": None},
+    ],
+}
+
+renjun = {
+    "id": 14,
+    "nome_artistico": "Renjun",
+    "nome": "Huang Renjun",
+    "ano_de_nascimento": "2000",
+    "nacionalidade": "chinês",
+    "debut_info": [{"ano": 2016, "unit": "dream", "debut_song": "Chewing Gum"}],
+}
+
+jeno = {
+    "id": 15,
+    "nome_artistico": "Jeno",
+    "nome": "Lee Jeno",
+    "ano_de_nascimento": "2000",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [{"ano": 2016, "unit": "dream", "debut_song": "Chewing Gum"}],
+}
+
+jaemin = {
+    "id": 16,
+    "nome_artistico": "Jaemin",
+    "nome": "Na Jaemin",
+    "ano_de_nascimento": "2000",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [{"ano": 2016, "unit": "dream", "debut_song": "Chewing Gum"}],
+}
+
+yangyang = {
+    "id": 17,
+    "nome_artistico": "Yangyang",
+    "nome": "Liu Yangyang",
+    "ano_de_nascimento": "2000",
+    "nacionalidade": "taiwanês",
+    "debut_info": [{"ano": 2019, "unit": "wayv", "debut_song": "Regular"}],
+}
+
+chenle = {
+    "id": 18,
+    "nome_artistico": "Chenle",
+    "nome": "Zhong Chenle",
+    "ano_de_nascimento": "2001",
+    "nacionalidade": "chinês",
+    "debut_info": [{"ano": 2016, "unit": "dream", "debut_song": "Chewing Gum"}],
+}
+
+jisung = {
+    "id": 19,
+    "nome_artistico": "Jisung",
+    "nome": "Park Jisung",
+    "ano_de_nascimento": "2002",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [{"ano": 2016, "unit": "dream", "debut_song": "Chewing Gum"}],
+}
+
+sion = {
+    "id": 20,
+    "nome_artistico": "Sion",
+    "nome": "Oh Sion",
+    "ano_de_nascimento": "2002",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [{"ano": 2024, "unit": "wish", "debut_song": "Wish"}],
+}
+
+riku = {
+    "id": 21,
+    "nome_artistico": "Riku",
+    "nome": "Maeda Riku",
+    "ano_de_nascimento": "2003",
+    "nacionalidade": "japonês",
+    "debut_info": [{"ano": 2024, "unit": "wish", "debut_song": "Wish"}],
+}
+
+yushi = {
+    "id": 22,
+    "nome_artistico": "Yushi",
+    "nome": "Tokuno Yushi",
+    "ano_de_nascimento": "2005",
+    "nacionalidade": "japonês",
+    "debut_info": [{"ano": 2024, "unit": "wish", "debut_song": "Wish"}],
+}
+
+jaehee = {
+    "id": 23,
+    "nome_artistico": "Jaehee",
+    "nome": "Kim Daeyoung",
+    "ano_de_nascimento": "2005",
+    "nacionalidade": "sul-coreano",
+    "debut_info": [{"ano": 2024, "unit": "wish", "debut_song": "Wish"}],
+}
+
+ryo = {
+    "id": 24,
+    "nome_artistico": "Ryo",
+    "nome": "Hirose Ryo",
+    "ano_de_nascimento": "2005",
+    "nacionalidade": "japonês",
+    "debut_info": [{"ano": 2024, "unit": "wish", "debut_song": "Wish"}],
+}
+
+sakuya = {
+    "id": 25,
+    "nome_artistico": "Sakuya",
+    "nome": "Fujinaga Sakuya",
+    "ano_de_nascimento": "2005",
+    "nacionalidade": "japonês",
+    "debut_info": [{"ano": 2024, "unit": "wish", "debut_song": "Wish"}],
+}
+
+#units
+dojaejung = {
+    "unit": "DOJAEJUNG",
+    "membros": [
+        doyoung, jaehyun, jungwoo
+    ],
+    "ano_de_debut": "2023",
+    "debut_song": "Perfume"
+},
+
+ilichil = {
+    "unit": "127",
+    "membros": [
+        johnny, taeyong, yuta, doyoung, jaehyun, jungwoo, mark, haechan
+    ],
+    "ano_de_debut": "2016",
+    "debut_song": "Fire Truck"
+}
+
+dream = {
+    "unit": "dream",
+    "membros": [
+        mark, haechan, renjun, jeno, jaemin, chenle, jisung
+    ],
+    "ano_de_debut": "2016",
+    "debut_song": "Chewing Gum"
+}
+
+wayv = {
+    "unit": "wayv",
+    "membros": [
+        kun, ten, winwin, hendery, xiaojun, yangyang
+    ],
+    "ano_de_debut": "2019",
+    "debut_song": "Regular"
+}
+
+wish = {
+    "unit": "wish",
+    "membros": [
+        sion, riku, yushi, jaehee, ryo, sakuya
+    ],
+    "ano_de_debut": "2024",
+    "debut_song": "Wish"
+}
+#nct
+nct = {
+    "units": [
+        dojaejung, ilichil, dream, wayv, wish
+    ],
+    "debut_years": ["2016", "2017", "2018", "2019", "2023", "2024"]
+}
 
 while True:
 
@@ -69,10 +345,12 @@ while True:
 
     id_membro = []
     membros_encontrados = []
+    units_encontradas = set()
+    anos_encontrados = []
 
     if opcao == OPCOES.ANO_DE_DEBUT.value:
         resposta = int(input("Digite o ano: "))
-        if resposta not in [int(ano) for ano in debut_years]:
+        if resposta not in [int(ano) for ano in nct["debut_years"]]:
             if resposta < 2016:
                 print(
                     "O NCT debutou, pela primeira vez, em 2016. Pesquise por anos posteriores a esse."
@@ -84,10 +362,13 @@ while True:
                 for membro in unit:
                     if isinstance(membro, list):
                         if resposta == int(membro[3]):
-                            if membro[0] not in id_membro and membro[1] not in membros_encontrados:
+                            if (
+                                membro[0] not in id_membro
+                                and membro[1] not in membros_encontrados
+                            ):
                                 id_membro.append(membro[0])
                                 membros_encontrados.append(membro[1])
-                                
+
         if len(membros_encontrados) >= 2:
             membros_encontrados.insert(-1, "e")
             lista_de_membros = ", ".join(membros_encontrados).replace(", e,", " e")
@@ -108,6 +389,7 @@ while True:
         elif resposta:
             for unit in nct:
                 if resposta.lower() == unit[0].lower():
+                    anos_encontrados.append(unit[-1])
                     if isinstance(unit, list):
                         for membro in unit:
                             if isinstance(membro, list):
@@ -118,9 +400,13 @@ while True:
             lista_de_membros = ", ".join(membros_encontrados).replace(", e,", " e")
 
             if resposta.lower() == wayv[0]:
-                print(f"Os membros do WayV são {lista_de_membros}.")
+                print(
+                    f"O WayV debutou em {wayv[-1]}. {lista_de_membros} são os membros do WayV."
+                )
             else:
-                print(f"Os membros do NCT {resposta.upper()} são {lista_de_membros}.")
+                print(
+                    f"O NCT {resposta.upper()} debutou em {anos_encontrados[0]}. {lista_de_membros} são os membros do NCT {resposta.upper()}."
+                )
 
     elif opcao == OPCOES.NOME.value:
         resposta = input("Digite o nome: ")
@@ -131,7 +417,6 @@ while True:
                         if resposta.lower() == membro[1].lower():
                             membros_encontrados.append(membro)
                             id_membro.append(membro[0])
-        units_encontradas = set()
 
         for membro in membros_encontrados:
             for unit in nct:
@@ -150,7 +435,7 @@ while True:
         if (len(units)) == 1 and "wayv" in units_encontradas:
             print(f"{resposta.capitalize()} está no WayV.")
         elif (len(units)) == 1 and "wayv" not in units_encontradas:
-            print(f"{resposta.capitalize()} está no NCT {(units[0])}.")
+            print(f"{resposta.capitalize()} está no NCT {(units[0].upper())}.")
         elif (len(units)) >= 2:
             print(f"{resposta.capitalize()} está no NCT {lista_de_units}.")
 
